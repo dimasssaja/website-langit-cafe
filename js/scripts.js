5432b3afdf8d073a109b5d100a48cb8c0571c935
dimasssaja <120650964+dimasssaja@users.noreply.github.com>
2023-12-04 00:05:20 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
